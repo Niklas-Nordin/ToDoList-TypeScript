0455b5f4cc37460815996dc4fc41fdc1e9eebf27
Niklas-Nordin <niklas.nordin@chasacademy.se>
2025-01-21 13:13:16 +0100

    delete button is working

--- script.js
+++ script.js
@@ -1,43 +1,90 @@
 var addTodo = document.getElementById("add-todo");
 var addBtn = document.getElementById("addBtn");
 var myTodos = document.getElementById("my-todos");
-var listItems = [
-    {
-        id: 1,
-        task: "hjälp grannen",
-        done: false,
-    },
-];
-var index = "";
-function addTask() {
-    if (addTodo.value === "") {
-        alert("You must write something!");
+var listItems = [];
+loadTodo();
+function addTask(todoItem) {
+    var addedTodo;
+    if (todoItem) {
+        addedTodo = todoItem;
     }
     else {
-        var li = document.createElement("li");
-        li.classList.add("li-item");
-        li.id = "".concat(listItems.length + 1);
-        li.innerHTML = "\n        <p class=\"todo\" id=\"".concat(listItems.length + 1, "\">").concat(addTodo.value, "</p>");
-        myTodos.append(li);
-        var addedTodo = {};
-        addedTodo.id = listItems.length + 1;
-        addedTodo.task = addTodo.value;
-        addedTodo.done = false;
+        if (addTodo.value === "") {
+            alert("You must write something!");
+            return;
+        }
+        addedTodo = {
+            id: listItems.length + 1,
+            task: addTodo.value,
+            done: false,
+        };
         listItems.push(addedTodo);
-        console.log(listItems);
-        addTodo.value = "";
     }
+    var li = document.createElement("li");
+    li.classList.add("li-item");
+    li.id = "".concat(addedTodo.id);
+    li.innerHTML = "\n            <input class=\"checkbox\" type=\"checkbox\">\n            <label class=\"todo\">".concat(addedTodo.task, "</label>\n            <input type=\"image\" src=\"./assets/images/edit.svg\" class=\"btn edit-btn\">\n            <input type=\"image\" src=\"./assets/images/delete.svg\" class=\"btn delete-btn\">");
+    myTodos.append(li);
+    saveTodo();
+    addTodo.value = "";
+    console.log(listItems);
+    console.log(addedTodo);
+    localStorage.setItem("user", JSON.stringify(listItems));
+    console.log("hej", localStorage.setItem("user", JSON.stringify(listItems)));
 }
-addBtn.addEventListener("click", addTask);
-// addBtn.addEventListener("keypress", function (event) {
-//     if(event.key)
-// });
+addBtn.addEventListener("click", function () { return addTask(null); });
 addTodo.addEventListener("keydown", function (event) {
     if (event.key === "Enter") {
-        addTask();
+        addTask(null);
     }
 });
-// function addTask() {
-//   if (todo.value === "") {
-//   }
-// }
+myTodos.addEventListener("click", function (event) {
+    var target = event.target;
+    var liItem = target.closest(".li-item");
+    var id = parseInt(liItem.id);
+    var selected = listItems.filter(function (todo) { return todo.id !== id; });
+    listItems = selected;
+    if (target.classList.contains("delete-btn")) {
+        liItem.remove();
+        // selected;
+        // listItems = selected;
+        console.log("listItems2", listItems);
+    }
+    else if (target.classList.contains("edit-btn")) {
+        liItem.style.backgroundColor = "blue";
+    }
+});
+//Checkbox, kontroller om done är true eller false
+myTodos.addEventListener("change", function (event) {
+    var target = event.target;
+    var liItem = target.closest(".li-item");
+    var id = parseInt(liItem.id);
+    var todo = listItems.filter(function (todo) { return todo.id === id; });
+    if (!todo) {
+        console.log("Couldn't find the id...");
+        return;
+    }
+    var matchedTodo = todo[0];
+    if (target.checked) {
+        matchedTodo.done = true;
+        liItem.style.opacity = "0.5";
+    }
+    else {
+        liItem.style.opacity = "1";
+        matchedTodo.done = false;
+    }
+});
+function loadTodo() {
+    var getStoredData = localStorage.getItem("user");
+    if (getStoredData) {
+        listItems = JSON.parse(getStoredData);
+        listItems.forEach(function (todo) {
+            addTask(todo);
+        });
+        console.log(listItems);
+    }
+    else {
+        console.log("no data");
+    }
+}
+function saveTodo() { }
--- script.ts
+++ script.ts
@@ -2,57 +2,109 @@ const addTodo = document.getElementById("add-todo") as HTMLInputElement;
 const addBtn = document.getElementById("addBtn") as HTMLButtonElement;
 const myTodos = document.getElementById("my-todos") as HTMLUListElement;
 
+let listItems: TodoItem[] = [];
+loadTodo();
+
 interface TodoItem {
   id: number;
   task: string;
   done: boolean;
 }
 
-let listItems: TodoItem[] = [
-  {
-    id: 1,
-    task: "hjälp grannen",
-    done: false,
-  },
-];
+function addTask(todoItem: TodoItem | null) {
+  let addedTodo: TodoItem;
 
-let index = "";
-
-function addTask() {
-  if (addTodo.value === "") {
-    alert("You must write something!");
+  if (todoItem) {
+    addedTodo = todoItem;
   } else {
-    const li = document.createElement("li");
-    li.classList.add("li-item");
-    li.id = `${listItems.length + 1}`;
-    li.innerHTML = `
-        <p class="todo" id="${listItems.length + 1}">${addTodo.value}</p>`;
-    myTodos.append(li);
+    if (addTodo.value === "") {
+      alert("You must write something!");
+      return;
+    }
 
-    let addedTodo = {} as TodoItem;
-    addedTodo.id = listItems.length + 1;
-    addedTodo.task = addTodo.value;
-    addedTodo.done = false;
+    addedTodo = {
+      id: listItems.length + 1,
+      task: addTodo.value,
+      done: false,
+    };
+
     listItems.push(addedTodo);
-    console.log(listItems);
-
-    addTodo.value = "";
   }
+
+  const li = document.createElement("li");
+  li.classList.add("li-item");
+  li.id = `${addedTodo.id}`;
+  li.innerHTML = `
+            <input class="checkbox" type="checkbox">
+            <label class="todo">${addedTodo.task}</label>
+            <input type="image" src="./assets/images/edit.svg" class="btn edit-btn">
+            <input type="image" src="./assets/images/delete.svg" class="btn delete-btn">`;
+  myTodos.append(li);
+  saveTodo();
+  addTodo.value = "";
+  console.log(listItems);
+  console.log(addedTodo);
+
+  localStorage.setItem("user", JSON.stringify(listItems));
 }
 
-addBtn.addEventListener("click", addTask);
-
-// addBtn.addEventListener("keypress", function (event) {
-//     if(event.key)
-// });
+addBtn.addEventListener("click", () => addTask(null));
 
 addTodo.addEventListener("keydown", function (event) {
   if (event.key === "Enter") {
-    addTask();
+    addTask(null);
   }
 });
 
-// function addTask() {
-//   if (todo.value === "") {
-//   }
-// }
+myTodos.addEventListener("click", (event) => {
+  const target = event.target as HTMLElement;
+  const liItem = target.closest(".li-item") as HTMLLIElement;
+  const id = parseInt(liItem.id);
+  const selected = listItems.filter((todo) => todo.id !== id);
+  listItems = selected;
+
+  if (target.classList.contains("delete-btn")) {
+    liItem.remove();
+    console.log("listItems2", listItems);
+  } else if (target.classList.contains("edit-btn")) {
+    liItem.style.backgroundColor = "blue";
+  }
+});
+
+//Checkbox, kontroller om done är true eller false
+
+myTodos.addEventListener("change", (event) => {
+  const target = event.target as HTMLInputElement;
+  const liItem = target.closest(".li-item") as HTMLLIElement;
+  let id = parseInt(liItem.id);
+  const todo = listItems.filter((todo) => todo.id === id);
+
+  if (!todo) {
+    console.log("Couldn't find the id...");
+    return;
+  }
+
+  const matchedTodo = todo[0];
+
+  if (target.checked) {
+    matchedTodo.done = true;
+    liItem.style.opacity = "0.5";
+  } else {
+    liItem.style.opacity = "1";
+    matchedTodo.done = false;
+  }
+});
+
+function loadTodo() {
+  let getStoredData: null | string = localStorage.getItem("user");
+
+  if (getStoredData) {
+    listItems = JSON.parse(getStoredData);
+    listItems.forEach((todo) => {
+      addTask(todo);
+    });
+    console.log(listItems);
+  } else {
+    console.log("no data");
+  }
+}
